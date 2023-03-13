8ad3f24e13fc010521a93f0c81a8de5f35ce9ae9
Guillermo F Funaro <funaro.g.f@gmail.com>
2023-03-13 15:58:27 -0300

    Carga: Total

--- Clase-5-Js/tareas.js
+++ Clase-5-Js/tareas.js
@@ -7,6 +7,12 @@ function counter() {
   // ejemplo: const newCounter = counter();
   // newCounter(); // 1
   // newCounter(); // 2
+  let num = 0;
+  const incrementador = function () {
+    num+=1;
+    return num;
+  }
+  return incrementador; 
 }
 
 function cacheFunction(cb) {
@@ -21,7 +27,17 @@ function cacheFunction(cb) {
   // si la invocas de nuevo con 5, deberia retornar 25 (guardado previament en el cache)
   // Tips, usá un objeto donde cada propiedad sea un argumento, y el valor el resultado.
   // usá hasOwnProperty!
+  let cache = {};
+  return function (argumento){
+    if (cache.hasOwnProperty(argumento)){
+      return cache[argumento];
+    }else{
+      cache[argumento] = cb(argumento);
+      return cache[argumento];
+    }
+  }
 }
+
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
